feb0c6f9b3e73454f73037ad619c38235aaf5ef4
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-01-28 14:41:32 -0500

    Time: 84 ms (22.36%), Space: 14.5 MB (81.60%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_s, dict_t = {}, {}
+        
+        for i in s:
+            if i in dict_s:
+                dict_s[i] += 1
+            else:
+                dict_s[i] = 1
+        
+        for j in t:
+            if j in dict_t:
+                dict_t[j] += 1
+            else:
+                dict_t[j] = 1
+        
+        if dict_s == dict_t:
+            return True
+        
+        return False
+        
