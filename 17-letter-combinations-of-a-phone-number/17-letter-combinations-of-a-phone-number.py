7b681f6afdc0d380b2712e08a52a90db9275cb81
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-01-31 17:15:14 -0500

    Time: 30 ms (77.81%), Space: 14 MB (95.96%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        # Define dictionary which links digit to characters.
+        digit_char = {2:['a','b','c'], 3:['d','e','f'], 4:['g','h','i'], 5:['j','k','l'], 6:['m','n','o'], 7:['p','q','r','s'], 8:['t','u','v'], 9:['w','x','y', 'z']}
+        if len(digits) == 0:
+            return []
+        
+        ans, string = [''], ""
+        for digit in digits:
+            ans = [i+j for i in ans for j in digit_char[int(digit)]]
+            # print(ans)  # digits = "23"  =>  ['a', 'b', 'c']
+                          #                    ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
+        
+        return ans
