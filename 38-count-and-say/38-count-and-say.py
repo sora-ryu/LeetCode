3327ad9ad63465f7c10ac6aa19dd42ef9aca565b
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-02-01 15:21:37 -0500

    Time: 138 ms (5.07%), Space: 14 MB (97.20%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -11,24 +11,6 @@ class Solution:
         "1113213211"
         #
 
-#         def directed_count_value(s):
-#             temp, count = '', 1
-#             for i in range(len(s) - 1):
-#                 if s[i] == s[i + 1]:
-#                     count += 1
-#                 else:
-#                     temp += str(count) + s[i]
-#                     count = 1
-
-#             temp += str(count) + s[-1]
-#             return temp
-
-#         result = '1'
-#         for _ in range(n - 1):
-#             result = directed_count_value(result)
-
-#         return result
-    
         if n == 1:
             return "1"
         
