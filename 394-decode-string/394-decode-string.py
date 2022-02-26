014e3e5dcdbf0e4701819a14adce28af313df5bd
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-02-25 23:49:41 -0500

    Time: 42 ms (52.47%), Space: 13.9 MB (59.92%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -18,18 +18,3 @@ class Solution:
                 print(stack)
         
         return ''.join(stack)
-        
-        
-#         stack = []
-#         for c in s:
-#             if c != ']': stack.append(c)
-#             else:
-#                 temp_str, temp_digit = '', ''
-#                 while stack[-1] != '[':
-#                     temp_str = stack.pop() + temp_str 
-#                 stack.pop() # pop '['
-                
-#                 while stack and stack[-1].isdigit(): # avoid pop out index when stack is empty 
-#                     temp_digit = stack.pop() + temp_digit
-#                 stack.append(int(temp_digit) * temp_str)
-#         return ''.join(stack)
