1ce01e269b64a8e97d4b6f543eb4942f33d6a244
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-02-25 23:48:55 -0500

    Time: 43 ms (49.77%), Space: 13.8 MB (87.49%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        stack = []
+        for c in s:
+            if c != ']':
+                stack.append(c)
+            else:  # If we meet ']', it's time to decode string
+                tmp_digit, tmp_str = '', ''
+                while stack[-1] != '[':
+                    tmp_str = stack.pop() + tmp_str
+                stack.pop()
+                print(tmp_str)
+                
+                while stack and stack[-1].isdigit():  # 100 => without this loop, recognize 1,0,0 / stack condition => do not pop out from the empty stack
+                    tmp_digit = stack.pop() + tmp_digit
+                stack.append(int(tmp_digit) * tmp_str)
+                print(stack)
+        
+        return ''.join(stack)
+        
+        
+#         stack = []
+#         for c in s:
+#             if c != ']': stack.append(c)
+#             else:
+#                 temp_str, temp_digit = '', ''
+#                 while stack[-1] != '[':
+#                     temp_str = stack.pop() + temp_str 
+#                 stack.pop() # pop '['
+                
+#                 while stack and stack[-1].isdigit(): # avoid pop out index when stack is empty 
+#                     temp_digit = stack.pop() + temp_digit
+#                 stack.append(int(temp_digit) * temp_str)
+#         return ''.join(stack)
