2eddb13630b7d4eea29254b4729a21d144023ae8
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-02-27 12:34:50 -0500

    Time: 292 ms (49.78%), Space: 14.4 MB (95.11%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.py
+++ 654-maximum-binary-tree/654-maximum-binary-tree.py
@@ -7,6 +7,10 @@
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
         
+        # Time Complexity: O(N^2)
+        # The worst case is when the nums array is sorted (ascending order)
+        # it takes O(N * N) while N is for finding maximum value, and
+        # another N is for constructing subtree for each level
         if nums:
             max_idx, max_val = max(enumerate(nums), key=lambda x: x[1])
             node = TreeNode(max_val)
