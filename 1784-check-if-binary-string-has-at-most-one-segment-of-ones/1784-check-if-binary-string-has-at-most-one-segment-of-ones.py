527c165d37a83e8b6c07d109a03862ad56a6a660
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-02-28 16:16:57 -0500

    Time: 44 ms (45.45%), Space: 13.9 MB (50.48%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,5 +1,8 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
         
+        # Without leading zeros
+        # => s doesn't start with 0
+        # => If "01" exists, it means that there is more than one segments of ones.
         return False if "01" in s else True
         
