9651be275bfce73e45f29509310490b6b0442ce8
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-03-02 23:29:01 -0500

    Time: 168 ms (70.65%), Space: 18.2 MB (57.23%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,24 +1,30 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        i = 0
+        # [1,3],[2,6] => [1,6]
+        # [1,6],[2,3] => [1,6]
+        # each interval is sorted?
+        # intervals are sorted? begin_i < begin_(i+1)? => No, [[1,4],[0,0]] => Let's sort first!
+        # is it possible to have start_i == end_i?
+        # merge if there's not space between end_i and start_(i+1)?
+        
         intervals.sort()
         
+        i = 0
+        # for-loop starting from 0 to len(intervals)-1 index
         while i < len(intervals)-1:
-            # print(i)
-            # print(intervals)
-            if intervals[i+1][0] <= intervals[i][1] and intervals[i+1][1] >= intervals[i][0]:
-                intervals[i][0] = min(intervals[i][0], intervals[i+1][0])
+            # consider only two intervals (ith and (i+1)th)
+            # if end_i < start_(i+1): it means that there's a space between two intervals
+            if intervals[i][1] < intervals[i+1][0]:
+                # do not merge
+                i += 1
+                continue
+            # else: merge
+            else:
                 intervals[i][1] = max(intervals[i][1], intervals[i+1][1])
+                intervals[i][0] = min(intervals[i][0], intervals[i+1][0])
                 del intervals[i+1]
-                i -= 1
-            
-            i += 1
-        
-        # final check
-        if len(intervals) >= 2 and intervals[-1][0] <= intervals[-2][1] and intervals[-1][1] >= intervals[-2][0]:
-            intervals[-2][0] = min(intervals[-2][0], intervals[-1][0])
-            intervals[-2][1] = max(intervals[-2][1], intervals[-1][1])
-            del intervals[-1]
         
         return intervals
+                
+        
